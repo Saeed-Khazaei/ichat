b3308d4bacb184752bb420f99ec78b281e05f179
Saeed <saeed@bookingvila.com>
2022-08-12 19:16:29 +0430

    add pages to the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
+import Chats from "./pages/Chats";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">HI</div>;
+  return (
+    <div className="App">
+      <Route path="/" exact component={Home} />
+      <Route path="/chats" component={Chats} />
+    </div>
+  );
 }
 
 export default App;
